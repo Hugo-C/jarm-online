db6422ed9dd101c6e729972006fcb1f1f5c648b2
Hugo-C <Hugo-C@users.noreply.github.com>
2022-10-02 01:06:57 +0200

    GH-7 # Add Sentry (#8)
    * Bump node to 16.17
    
    * Add Sentry to Vue
    
    * Bump Rust to 1.64
    
    * Add Sentry to API

--- jarm_online_gui/src/main.js
+++ jarm_online_gui/src/main.js
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import Equal from 'equal-vue'
 import 'equal-vue/dist/style.css'
 import { createMetaManager } from 'vue-meta'
+import * as Sentry from "@sentry/vue"
 import App from './App.vue'
 
 const app = createApp(App)
@@ -21,7 +22,14 @@ app.component('todo-item', {
     template: `<li>{{ todo.text }}</li>`
 })
 
-
+// Set at build time through env var
+// see https://cli.vuejs.org/guide/mode-and-env.html#using-env-variables-in-client-side-code
+let sentry_dsn = process.env.VUE_APP_SENTRY_DSN
+Sentry.init({
+  app,
+  dsn: sentry_dsn,
+  tracesSampleRate: 1.0,
+});
 
 app
     .use(Equal)
